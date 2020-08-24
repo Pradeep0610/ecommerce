c0af444a2dbba8d72c47bc1460b0ab73e820f893
Pradeep Subedi <cenapradeep94@gmail.com>
2020-08-24 19:13:54 +0545

    Fixed Modal close bug

--- admin/src/components/AddNewOwnerModal.vue
+++ admin/src/components/AddNewOwnerModal.vue
@@ -2,25 +2,29 @@
 <b-modal id="add-owner-modal" ref="modal" title="Add a new Owner" @show="resetModal" @hide="resetModal" hide-footer>
     <ValidationObserver v-slot="{ handleSubmit }">
         <b-form @submit.prevent="handleSubmit(onSubmit)">
+            <!-- Name input -->
             <validation-provider name="Name" rules="required" v-slot="{ valid, errors }">
                 <b-form-group label="Name" label-for="name-input" invalid-feedback="Name is required">
                     <b-form-input id="name-input" :state="errors[0] ? false : (valid ? true : null)" v-model="name"></b-form-input>
                 </b-form-group>
             </validation-provider>
 
+            <!-- About input -->
             <validation-provider name="About" rules="required" v-slot="{ valid, errors }">
                 <b-form-group label="About" label-for="about-input" invalid-feedback="About is required">
                     <b-form-textarea id="about-input" :state="errors[0] ? false : (valid ? true : null)" size="default" placeholder="Describe about owner" v-model="about"></b-form-textarea>
                 </b-form-group>
             </validation-provider>
 
+            <!-- Photo input -->
             <validation-provider name="Photo" rules="required" v-slot="{ validate, errors }">
                 <b-form-group label="Photo" label-for="photo-input" invalid-feedback="Photo is required">
                     <b-form-file placeholder="Choose a file or drop it here..." drop-placeholder="Drop file here..." :state="errors[0] ? false : (validate ? true : null)" @input="validate" v-model="selectedFile"></b-form-file>
                 </b-form-group>
             </validation-provider>
 
-            <b-button type="submit" @click="onSubmit" variant="success">Add Owner</b-button>
+            <!-- Buttons -->
+            <b-button type="submit" variant="success">Add Owner</b-button>
             <b-button @click="closeModal" variant="light">Cancel</b-button>
         </b-form>
     </ValidationObserver>
